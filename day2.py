0fdf986f8c91d96cf18b1de5c1ae413c766d4eb5
Ridhin Shalu <contactridhin@gmail.com>
2026-05-06 13:18:43 -0400

    Day 2: API call and data extraction

---
+++ day2.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://api.github.com/users/contactridhin-netizen"
+
+response = requests.get(url)
+data = response.json()
+
+print("Username:", data["login"])
+print("Public repos:", data["public_repos"])
